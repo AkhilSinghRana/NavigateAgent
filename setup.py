2cc3993cf1590bf388f476b61a9a6092edbaafe8
Tatjana Bürgmann <tatjana.buergmann@gmail.com>
2019-06-21 15:11:19 +0200

    Addes Setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import os 
+from setuptools import setup, find_packages
+
+
+#Use README file for long description of the project
+##     RootDir
+###    - README.md
+###    - setup.py
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+def parse_requiremetns(fname):
+    with open(fname) as f:
+        required = f.read().splitlines()
+    print(required)
+    return required
+setup(
+    name = "NavigateAgent",
+    author = "Akhil Singh Rana",
+    author_email = "er.akhil.singh.rana@gmail.com",
+    description = ("This is a project from UDacity nanodegree program"
+                    "Advanced  Reinforcement Learning"),
+    long_description = read("README.md"),
+    install_requires = parse_requiremetns("requirements.txt"),    packages=find_packages()
+)
