153f7204f310f7a6d2983076aaebf2782542afe2
AkhilSinghRana <akhilsinghrana@gmail.com>
2019-06-22 19:42:04 +0200

    Modified latest checkpoints

--- DQN/dqn_agent.py
+++ DQN/dqn_agent.py
@@ -9,10 +9,10 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 BUFFER_SIZE = int(1e5)  # replay buffer size
-BATCH_SIZE = 128         # minibatch size
+BATCH_SIZE = 64         # minibatch size
 GAMMA = 0.99            # discount factor
 TAU = 1e-3              # for soft update of target parameters
-LR = 4e-4               # learning rate 
+LR = 5e-4               # learning rate 
 UPDATE_EVERY = 4        # how often to update the network
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
